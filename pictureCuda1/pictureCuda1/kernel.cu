4db091dbdbdccd9abce9977c901b622122579942
ryanmahoney96 <ryan.mahoney96@gmail.com>
2017-01-13 12:00:43 -0600

    day9

--- pictureCuda1/pictureCuda1/kernel.cu
+++ pictureCuda1/pictureCuda1/kernel.cu
@@ -1,4 +1,9 @@
 
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include "highPerformanceTimer.h"
+
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui//highgui.hpp>
@@ -8,13 +13,30 @@
 using namespace cv;
 using namespace std;
 
-void threshold (int threshold, int w, int height, unsigned char* data);
+/*struct threshData {
+	int* threshNum;
+	Mat* image;
+};*/
+
+__global__ void threshKernel()
+{
+	int i = blockIdx.x * blockDim.x + threadIdx.x;
+	//int i = threadIdx.x;
+
+}
+
+double CPUthreshold (int threshold, int w, int height, unsigned char* data);
+double GPUthreshold(int threshold, Mat* image);
+
+//void on_trackbar (int thresholdNum, void* td);
 
 typedef unsigned char uchar;
 
 int main(int argc, char * argv[])
 {
 	//int endRet = 0;
+	int thresholdNum = 128;
+	//const int threshMAX = 255;
 
 	if (argc != 2) {
 		cout << "Usage: display_image ImageToLoadAndDisplay" << endl;
@@ -28,6 +50,9 @@ int main(int argc, char * argv[])
 	//read the file from the command line
 	image = imread(argv[1], CV_LOAD_IMAGE_COLOR);
 	
+	/*threshData td;
+	td.threshNum = &thresholdNum;
+	td.image = &image;*/
 
 	//check for an invalid input
 	//Mat.data is a pointer to the image data -> if null, no image found/useable
@@ -42,22 +67,40 @@ int main(int argc, char * argv[])
 	cvtColor(image, image, cv::COLOR_RGB2GRAY);
 	cout << "Image converted to Grayscale" << endl;
 
-	threshold(128, image.cols, image.rows, image.data);
+	double CPUTime = CPUthreshold(thresholdNum, image.cols, image.rows, image.data);
+	double GPUTime = GPUthreshold(thresholdNum, &image);
 
 	//create a window for display
 	namedWindow("Display Window", WINDOW_NORMAL);
+
 	//show the image within the display window
 	imshow("Display Window", image);
 
+	//the trackbar must be placed inside a window
+	//whenever the user changes the trackbar, the on_trackbar function is called
+	//trackbar name, window name, an int to change(?), a maximum value(?), and a function to call
+	//createTrackbar("Threshold Slider", "Display Window", &thresholdNum, threshMAX, on_trackbar, &td);
+
+	//on_trackbar(thresholdNum, &td);
+
 	//wait for the user to enter a keystroke
+	cout << "The CPU took " << CPUTime << " seconds to render the threshold on the image" << endl;
+	cout << "The GPU took " << GPUTime << " seconds to render the threshold on the image" << endl;
+
 	waitKey(0);
 
 
     return 0;
 }
 
-void threshold(int threshold, int width, int height, unsigned char* data) {
+double CPUthreshold(int threshold, int width, int height, unsigned char* data) {
 	
+	//for CPU version, time this loop
+	HighPrecisionTime render;
+	double renderTime = 0.0;
+
+	render.TimeSinceLastCall();
+
 	for (uchar* i = data; i < &data[width*height]; i++) {
 
 		if (*i > threshold) {
@@ -69,4 +112,35 @@ void threshold(int threshold, int width, int height, unsigned char* data) {
 
 	}
 
+	renderTime = render.TimeSinceLastCall();
+
+	return renderTime;
 }
+
+double GPUthreshold(int threshold, Mat* image) {
+
+	HighPrecisionTime render;
+	double renderTime = 0.0;
+
+	cudaDeviceProp prop;
+	cudaGetDeviceProperties(&prop, 0);
+
+	int maxThreadsPerBlock = prop.maxThreadsPerBlock;
+	int numberOfBlocks = (image->cols * image->rows) / maxThreadsPerBlock + 1;
+
+	cout << maxThreadsPerBlock << endl << numberOfBlocks << endl;
+
+
+	
+	return renderTime;
+}
+
+/*void on_trackbar(int thresholdNum, void* td) {
+
+	Mat* img = ((threshData*)td)->image;
+
+	threshold(*td->thresholdNum, img->cols, img->rows, img->data);
+
+	imshow("Display Window", *img);
+
+}*/
