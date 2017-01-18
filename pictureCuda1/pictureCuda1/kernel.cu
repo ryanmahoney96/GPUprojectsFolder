23e5f589d5679611c53fd5a7bc6198ef02062d6d
ryanmahoney96 <ryan.mahoney96@gmail.com>
2017-01-18 11:53:52 -0600

    day 12

--- pictureCuda1/pictureCuda1/kernel.cu
+++ pictureCuda1/pictureCuda1/kernel.cu
@@ -15,10 +15,11 @@ using namespace std;
 
 typedef unsigned char uchar;
 
-/*struct threshData {
-	int* threshNum;
-	Mat* image;
-};*/
+const char* cpuWindow = "CPU Window";
+const char* gpuWindow = "GPU Window";
+
+Mat image;
+Mat GPUImage;
 
 __global__ void threshKernel(uchar* imageData, size_t size_of_image, int threshold)
 {
@@ -39,29 +40,29 @@ __global__ void threshKernel(uchar* imageData, size_t size_of_image, int thresho
 
 }
 
+double cpuBoxFilter(uchar* src, uchar*& dst, int width, int height, uchar* k, int kerWidth, int kerHeight, uchar* tmp);
+
 double CPUthreshold (int threshold, int w, int height, unsigned char* data);
 double GPUthreshold(int threshold, Mat* image, Mat& renderedImage);
 
-//void on_trackbar (int thresholdNum, void* td);
+void on_cpu_trackbar (int cpuThresholdNum, void*);
+void on_gpu_trackbar(int gpuThresholdNum, void*);
+
 
 int main(int argc, char * argv[])
 {
-	int thresholdNum = 128;
-	//const int threshMAX = 255;
+	int cpuThresholdNum = 128;
+	int gpuThresholdNum = 128;
+
+	const int threshMAX = 255;
 
 	try {
 		if (argc != 2) {
 			throw("Usage: display_image ImageToLoadAndDisplay");
 		}
 
-
-		Mat image;
 		//read the file from the command line
 		image = imread(argv[1], CV_LOAD_IMAGE_COLOR);
-
-		/*threshData td;
-		td.threshNum = &thresholdNum;
-		td.image = &image;*/
 
 		//check for an invalid input
 		//Mat.data is a pointer to the image data -> if null, no image found/useable
@@ -75,31 +76,49 @@ int main(int argc, char * argv[])
 		cvtColor(image, image, cv::COLOR_RGB2GRAY);
 		cout << "Image converted to Grayscale" << endl;
 
-		Mat GPUImage = image.clone();
+		GPUImage = image.clone();
 
-		double CPUTime = CPUthreshold(thresholdNum, image.cols, image.rows, image.data);
-		double GPUTime = GPUthreshold(thresholdNum, &image, GPUImage);
+		Mat cputmp = image.clone();
+		Mat gputmp = image.clone();
+
+		double CPUTime = 0;
+		double GPUTime = 0;
+
+		uchar boxKernel[] = { 1, 1, 1, 
+							  1, 3, 1, 
+							  1, 1, 1 
+							};
+
+		//NOTE: change hardcoded 3's, change last parameter to useful data
+		CPUTime = cpuBoxFilter(image.data, cputmp.data, image.cols, image.rows, boxKernel, 3, 3, 0);
+
+		//thresholds
+		//CPUTime = CPUthreshold(cpuThresholdNum, cputmp.cols, cputmp.rows, cputmp.data);
+		//GPUTime = GPUthreshold(gpuThresholdNum, &cputmp, gputmp);
 
 		//create a window for display
-		namedWindow("CPU Display Window", WINDOW_NORMAL);
-		namedWindow("GPU Display Window", WINDOW_NORMAL);
+		namedWindow(cpuWindow, WINDOW_NORMAL);
+		namedWindow(gpuWindow, WINDOW_NORMAL);
 
 		//show the image within the display window
-		imshow("CPU Display Window", image);
-		imshow("GPU Display Window", GPUImage);
+		imshow(cpuWindow, cputmp);
+		imshow(gpuWindow, image);
 
+		//trackbars
 		//the trackbar must be placed inside a window
 		//whenever the user changes the trackbar, the on_trackbar function is called
 		//trackbar name, window name, an int to change(?), a maximum value(?), and a function to call
-		//createTrackbar("Threshold Slider", "Display Window", &thresholdNum, threshMAX, on_trackbar, &td);
+		/*createTrackbar("Threshold", cpuWindow, &cpuThresholdNum, threshMAX, on_cpu_trackbar);
+		createTrackbar("Threshold", gpuWindow, &gpuThresholdNum, threshMAX, on_gpu_trackbar);
 
-		//on_trackbar(thresholdNum, &td);
+		on_cpu_trackbar(cpuThresholdNum, 0);
+		on_gpu_trackbar(gpuThresholdNum, 0);*/
 
 		//wait for the user to enter a keystroke
 		cout << "The CPU took " << CPUTime << " seconds to render the threshold on the image" << endl;
 		cout << "The GPU took " << GPUTime << " seconds to render the threshold on the image" << endl;
 
-		cout << "The GPU is " << CPUTime / GPUTime << " times faster than the CPU" << endl;
+		//cout << "The GPU is " << CPUTime / GPUTime << " times faster than the CPU" << endl;
 
 		waitKey(0);
 	}
@@ -134,7 +153,7 @@ double CPUthreshold(int threshold, int width, int height, unsigned char* data) {
 	return renderTime;
 }
 
-double GPUthreshold(int threshold, Mat* image, Mat& renderedImage) {
+double GPUthreshold(int threshold, Mat* img, Mat& renderedImage) {
 
 	HighPrecisionTime render;
 	HighPrecisionTime copying;
@@ -150,9 +169,9 @@ double GPUthreshold(int threshold, Mat* image, Mat& renderedImage) {
 	cudaGetDeviceProperties(&prop, 0);
 
 	int maxThreadsPerBlock = prop.maxThreadsPerBlock;
-	int numberOfBlocks = (image->cols * image->rows) / maxThreadsPerBlock + 1;
+	int numberOfBlocks = (img->cols * img->rows) / maxThreadsPerBlock + 1;
 
-	size_t size_of_image = image->cols * image->rows * sizeof(uchar);
+	size_t size_of_image = img->cols * img->rows * sizeof(uchar);
 
 	// Choose which GPU to run on, change this on a multi-GPU system.
 	cudaStatus = cudaSetDevice(0);
@@ -167,7 +186,7 @@ double GPUthreshold(int threshold, Mat* image, Mat& renderedImage) {
 		goto Error;
 	}
 
-	cudaStatus = cudaMemcpy((void*)GPUImageData, (void*)image->data, size_of_image, cudaMemcpyHostToDevice);
+	cudaStatus = cudaMemcpy((void*)GPUImageData, (void*)img->data, size_of_image, cudaMemcpyHostToDevice);
 	if (cudaStatus != cudaSuccess) {
 		throw("cudaMemcpy of image from CPU to GPU failed!");
 		goto Error;
@@ -175,7 +194,7 @@ double GPUthreshold(int threshold, Mat* image, Mat& renderedImage) {
 
 	/*1024
 	32401*/
-	cout << maxThreadsPerBlock << endl << numberOfBlocks << endl;
+	//cout << maxThreadsPerBlock << endl << numberOfBlocks << endl;
 
 	render.TimeSinceLastCall();
 
@@ -216,12 +235,89 @@ Error:
 	return renderTime;
 }
 
-/*void on_trackbar(int thresholdNum, void* td) {
+void on_cpu_trackbar(int cpuThresholdNum, void*) {
 
-	Mat* img = ((threshData*)td)->image;
+	Mat cputmp = GPUImage.clone();
 
-	threshold(*td->thresholdNum, img->cols, img->rows, img->data);
+	double CPUTime = CPUthreshold(cpuThresholdNum, cputmp.cols, cputmp.rows, cputmp.data);
 
-	imshow("Display Window", *img);
+	cout << "CPU Threshold Number: " << cpuThresholdNum << endl;
 
-}*/
+	imshow(cpuWindow, cputmp);
+}
+
+void on_gpu_trackbar(int gpuThresholdNum, void*) {
+
+	Mat cputmp = GPUImage.clone();
+	Mat gputmp = GPUImage.clone();
+
+	double GPUTime = GPUthreshold(gpuThresholdNum, &cputmp, gputmp);
+
+	cout << "GPU Threshold Number: " << gpuThresholdNum << endl;
+
+	imshow(gpuWindow, gputmp);
+
+}
+
+// CPUTime = cpuBoxFilter(image.data, cputmp.data, image.cols, image.rows, boxKernel, 3, 3, 0);
+
+double cpuBoxFilter(uchar* src, uchar*& dst, int width, int height, uchar* k, int kerWidth, int kerHeight, uchar* tmp) {
+
+	HighPrecisionTime filter;
+	double filterTime = 0.0;
+	
+	//the sum of the prox pixels after filtering
+	double currentSum = 0.0;
+
+	double avgNum = 0.0;
+	double numCells = 0;
+
+	for (int it = 0; it < (numCells = kerWidth * kerHeight); it++) {
+		avgNum += k[it];
+	}
+
+	//avgNum = avgNum / numCells;
+
+	//start on pixel
+	//find the pixels in proximity
+	//multiply their b/w values by corresponding filter val
+	//sum all prox pixels
+	//divide by sum of vals in kernel array
+	//save new value in dst.data[i]
+	//move to next pixel, continue
+
+	//the direction one needs to move in order to get to the desired border pixel relative to the current pixel
+	//NOTE: generalize this using the dimensions of the kernel
+	//
+	int indices [] = {
+		-(width + 1),  -width,     -(width - 1),
+		-1,                0,           +1,
+		width - 1,      width,      width + 1
+	};
+
+	filter.TimeSinceLastCall();
+
+	//roll through every pixel in the image using this loop
+	for (int i = width; i < (width*height - width); i++) {
+
+		currentSum = 0.0;
+
+		//if edge pixel
+		if (i % width == 0 || (i % width) - 1 == 0 ) {
+			continue;
+		}
+
+		//roll through each kernel corresponding pixel surrounding the current pixel (i)
+		for (int j = 0; j < numCells; j++) {
+			currentSum += src[i + indices[j]] * k[j];
+		}
+
+		dst[i] = currentSum / avgNum;
+
+	}
+
+
+	filterTime = filter.TimeSinceLastCall();
+
+	return filterTime;
+}
