8aa40b333dbf51546280be13344200d7cae4cc70
ryanmahoney96 <ryan.mahoney96@gmail.com>
2017-01-24 12:00:21 -0600

    jan24

--- GPUprojectsFolder/pictureCuda1/pictureCuda1/kernel.cu
+++ GPUprojectsFolder/pictureCuda1/pictureCuda1/kernel.cu
@@ -52,12 +52,11 @@ __global__ void bfKernel(uchar* src, uchar* dst, int width, int height, int* k, 
 
 	float avgNum = 0.0f;
 
+	//the current pixel in the image, using the dimensions of the image
+	const int currentPixel = y * width + x;
+
 	for (int it = 0; it < (kerWidth * kerHeight); it++) {
-<<<<<<< HEAD
 		avgNum += k[it]; 
-=======
-		avgNum += k[it];
->>>>>>> origin/master
 	}
 
 	//don't divide by 0 down below
@@ -65,36 +64,40 @@ __global__ void bfKernel(uchar* src, uchar* dst, int width, int height, int* k, 
 		avgNum = 1;
 	}
 
-	int coeff = kerWidth / 2;
+	const int coeff = kerWidth / 2;
 
-	//check to make sure its NOT an edge pixel
-	//if (x - coeff < 0 || x + coeff > width || y - coeff < 0 || y + coeff > height) {
-	//	//
-	//}
+	//check to make sure its NOT an edge pixel: left, right, top, bottom
+	if ((x <= coeff) || (x % width < coeff) || (currentPixel / width < coeff) || (currentPixel / width > height - coeff)) {
+		dst[currentPixel] = src[currentPixel];
+	}
 
-	//else {
+	else {
 
 		//roll through each kernel corresponding pixel surrounding the current pixel
-		int currentPixel = y * width + x;
+
+		//could use loop unrolling to optimize these loops using the inverse of signs 
 
 		int j = 0;
 
+		//m is essentially the vertical distance the desired pixel is from the origin
 		for (int m = -coeff; m <= coeff; m++) {
+			//c is the horizontal distance the desired pixel is from the origin
 			for (int c = -coeff; c <= coeff; c++) {
-				//i is the current pixel. We find the pixels around i by multiplying the number of widths away the desired modifying pixel is from origin by width, plus the number of pixels horizontally
+				//We find the pixels around the origin by multiplying the number of widths away the desired modifying pixel is from origin, plus the number of pixels horizontally
+				//then we multiply the value at that pixel by the corresponding index in the kernel
 				currentSum += src[currentPixel + (m * width + c)] * k[j];
 				j++;
 			}
 		}
 
-		dst[currentPixel] = (uchar)abs((currentSum / avgNum));
+		dst[currentPixel] = (uchar)(currentSum / avgNum);
 
-	//}
+	}
 }
 
 double cpuBoxFilter(uchar* src, uchar*& dst, int width, int height, int* k, int kerWidth, int kerHeight, uchar* tmp);
 
-double GPUBoxFilter(Mat* src, Mat& dst, int width, int height, int* k, int kerWidth, int kerHeight);
+double GPUBoxFilter(uchar* src, uchar*& dst, int width, int height, int* k, int kerWidth, int kerHeight);
 
 double CPUthreshold (int threshold, int w, int height, unsigned char* data);
 double GPUthreshold(int threshold, Mat* image, Mat& renderedImage);
@@ -138,6 +141,7 @@ int main(int argc, char * argv[])
 		double CPUTime = 0;
 		double GPUTime = 0;
 
+		//one side of the kernel is ___ long/high
 		const int arraySize = 9;
 
 		int boxKernel[arraySize* arraySize];
@@ -154,13 +158,13 @@ int main(int argc, char * argv[])
 			0, 0, 0, 0, 0, 255
 			};*/
 
-		for (int i = 0; i < arraySize * arraySize; i++) {
+		for (int i = 0; i < (arraySize * arraySize); i++) {
 			boxKernel[i] = 1;
 		}
 
 		//NOTE: change last parameter to useful data
 		CPUTime = cpuBoxFilter(image.data, cputmp.data, image.cols, image.rows, boxKernel, arraySize, arraySize, 0);
-		GPUTime = GPUBoxFilter(&image, gputmp, image.cols, image.rows, boxKernel, arraySize, arraySize);
+		GPUTime = GPUBoxFilter(image.data, gputmp.data, image.cols, image.rows, boxKernel, arraySize, arraySize);
 
 		//thresholds
 		//CPUTime = CPUthreshold(cpuThresholdNum, cputmp.cols, cputmp.rows, cputmp.data);
@@ -192,7 +196,7 @@ int main(int argc, char * argv[])
 		cout << "The CPU took " << CPUTime << " seconds to render the effect on the image" << endl;
 		cout << "The GPU took " << GPUTime << " seconds to render the effect on the image" << endl;
 
-		//cout << "The GPU is " << CPUTime / GPUTime << " times faster than the CPU" << endl;
+		cout << "In this run, the GPU ran " << (float)CPUTime / (float)GPUTime << " times faster than the CPU" << endl;
 
 		waitKey(0);
 	}
@@ -309,108 +313,6 @@ Error:
 	return renderTime;
 }
 
-double GPUBoxFilter(Mat* src, Mat& dst, int width, int height, int* k, int kerWidth, int kerHeight) {
-
-	HighPrecisionTime render;
-	HighPrecisionTime copying;
-	double renderTime = 0.0;
-	double copyTime = 0.0;
-
-	//consider pass by reference
-	//Mat GPUImage = (*image).clone();
-
-	uchar* GPUImageData;
-	uchar* tmp;
-
-	int* gpu_k;
-
-	cudaError_t cudaStatus;
-	cudaDeviceProp prop;
-	cudaGetDeviceProperties(&prop, 0);
-
-	int numPixels = width * height;
-
-	int maxThreadsPerBlock = prop.maxThreadsPerBlock;
-	int numberOfBlocks = numPixels / maxThreadsPerBlock + 1;
-
-	size_t size_of_image = numPixels * sizeof(uchar);
-
-	// Choose which GPU to run on, change this on a multi-GPU system.
-	cudaStatus = cudaSetDevice(0);
-	if (cudaStatus != cudaSuccess) {
-		throw("cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
-		goto Error;
-	}
-
-	cudaStatus = cudaMalloc((void**)&GPUImageData, size_of_image);
-	if (cudaStatus != cudaSuccess) {
-		throw("cudaMalloc of image failed!");
-		goto Error;
-	}
-
-	cudaStatus = cudaMalloc((void**)&tmp, size_of_image);
-	if (cudaStatus != cudaSuccess) {
-		throw("cudaMalloc of image failed!");
-		goto Error;
-	}
-
-	cudaStatus = cudaMalloc((void**)&gpu_k, kerHeight * kerWidth);
-	if (cudaStatus != cudaSuccess) {
-		throw("cudaMalloc of image failed!");
-		goto Error;
-	}
-
-	cudaStatus = cudaMemcpy((void*)GPUImageData, (void*)src->data, size_of_image, cudaMemcpyHostToDevice);
-	if (cudaStatus != cudaSuccess) {
-		throw("cudaMemcpy of image from CPU to GPU failed!");
-		goto Error;
-	}
-
-	cudaStatus = cudaMemcpy((void*)gpu_k, (void*)k, kerHeight * kerWidth, cudaMemcpyHostToDevice);
-	if (cudaStatus != cudaSuccess) {
-		throw("cudaMemcpy of image from CPU to GPU failed!");
-		goto Error;
-	}
-
-	render.TimeSinceLastCall();
-
-	//The first argument in the execution configuration specifies the number of thread blocks in the grid, and the second specifies the number of threads in a thread block.
-	bfKernel <<< numberOfBlocks, maxThreadsPerBlock >>> (GPUImageData, tmp, width, height, gpu_k, kerWidth, kerHeight);
-
-	// cudaDeviceSynchronize waits for the kernel to finish, and returns
-	// any errors encountered during the launch.
-	cudaStatus = cudaDeviceSynchronize();
-	if (cudaStatus != cudaSuccess) {
-		fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching bfKernel!\n", cudaStatus);
-		goto Error;
-	}
-
-	renderTime = render.TimeSinceLastCall();
-
-	// Check for any errors launching the kernel
-	cudaStatus = cudaGetLastError();
-	if (cudaStatus != cudaSuccess) {
-		fprintf(stderr, "bfKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
-		goto Error;
-	}
-
-
-
-	// Copy output vector from GPU buffer to host memory.
-	cudaStatus = cudaMemcpy((void*)dst.data, tmp, size_of_image, cudaMemcpyDeviceToHost);
-	if (cudaStatus != cudaSuccess) {
-		throw("cudaMemcpy of image data from GPU to CPU failed!");
-		goto Error;
-	}
-
-
-
-Error:
-	cudaFree(GPUImageData);
-
-	return renderTime;
-}
-
 void on_cpu_trackbar(int cpuThresholdNum, void*) {
 
 	Mat cputmp = GPUImage.clone();
@@ -434,8 +336,6 @@ void on_gpu_trackbar(int gpuThresholdNum, void*) {
 	imshow(gpuWindow, gputmp);
 
 }
-
-// CPUTime = cpuBoxFilter(image.data, cputmp.data, image.cols, image.rows, boxKernel, 3, 3, 0);
 
 double cpuBoxFilter(uchar* src, uchar*& dst, int width, int height, int* k, int kerWidth, int kerHeight, uchar* tmp) {
 
@@ -504,4 +404,113 @@ double cpuBoxFilter(uchar* src, uchar*& dst, int width, int height, int* k, int 
 	filterTime = filter.TimeSinceLastCall();
 
 	return filterTime;
+}
+
+double GPUBoxFilter(uchar* src, uchar*& dst, int width, int height, int* k, int kerWidth, int kerHeight) {
+
+	//measuring how long it takes to render the image effect and copy the information from CPU/GPU
+	HighPrecisionTime render;
+	HighPrecisionTime copying;
+	double renderTime = 0.0;
+	double copyTime = 0.0;
+
+	//these are variables that will be stored and used on the GPU
+	uchar* GPUImageData;
+	uchar* tmp;
+	int* gpu_k;
+
+	cudaError_t cudaStatus;
+	cudaDeviceProp prop;
+	cudaGetDeviceProperties(&prop, 0);
+
+	//the total number of pixels in the image, using the dimensions
+	int numPixels = width * height;
+
+	//the size, in bytes, that the kernel requires
+	size_t size_of_kernel = kerHeight * kerWidth * sizeof(int);
+
+	int maxThreadsPerBlock = prop.maxThreadsPerBlock;
+	int numberOfBlocks = numPixels / maxThreadsPerBlock + 1;
+
+	//the size of the image data, in bytes
+	size_t size_of_image = numPixels * sizeof(uchar);
+
+	// Choose which GPU to run on, change this on a multi-GPU system.
+	cudaStatus = cudaSetDevice(0);
+	if (cudaStatus != cudaSuccess) {
+		throw("cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
+		goto Error;
+	}
+
+	//
+	//Carving out room for three variables that will be stored and used on the gpu. Their size is dependent on the size of their CPU counterparts
+	//
+	cudaStatus = cudaMalloc((void**)&GPUImageData, size_of_image);
+	if (cudaStatus != cudaSuccess) {
+		throw("cudaMalloc of image failed!");
+		goto Error;
+	}
+
+	cudaStatus = cudaMalloc((void**)&tmp, size_of_image);
+	if (cudaStatus != cudaSuccess) {
+		throw("cudaMalloc of image failed!");
+		goto Error;
+	}
+
+	cudaStatus = cudaMalloc((void**)&gpu_k, size_of_kernel);
+	if (cudaStatus != cudaSuccess) {
+		throw("cudaMalloc of image failed!");
+		goto Error;
+	}
+
+
+	//Copying the relevant information from the CPU variables to their GPU counterparts
+	cudaStatus = cudaMemcpy((void*)GPUImageData, (void*)src, size_of_image, cudaMemcpyHostToDevice);
+	if (cudaStatus != cudaSuccess) {
+		throw("cudaMemcpy of image from CPU to GPU failed!");
+		goto Error;
+	}
+
+	cudaStatus = cudaMemcpy((void*)gpu_k, (void*)k, size_of_kernel, cudaMemcpyHostToDevice);
+	if (cudaStatus != cudaSuccess) {
+		throw("cudaMemcpy of image from CPU to GPU failed!");
+		goto Error;
+	}
+
+	//start timing how long it takes to render the effect
+	render.TimeSinceLastCall();
+
+	//The first argument in the execution configuration specifies the number of thread blocks in the grid, and the second specifies the number of threads in a thread block.
+	bfKernel << < numberOfBlocks, maxThreadsPerBlock >> > (GPUImageData, tmp, width, height, gpu_k, kerWidth, kerHeight);
+
+	// cudaDeviceSynchronize waits for the kernel to finish, and returns
+	// any errors encountered during the launch.
+	cudaStatus = cudaDeviceSynchronize();
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching bfKernel!\n", cudaStatus);
+		goto Error;
+	}
+
+	renderTime = render.TimeSinceLastCall();
+
+	// Check for any errors launching the kernel
+	cudaStatus = cudaGetLastError();
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "bfKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
+		goto Error;
+	}
+
+	// Copy output vector from GPU buffer to host memory.
+	cudaStatus = cudaMemcpy((void*)dst, tmp, size_of_image, cudaMemcpyDeviceToHost);
+	if (cudaStatus != cudaSuccess) {
+		throw("cudaMemcpy of image data from GPU to CPU failed!");
+		goto Error;
+	}
+
+
+
+Error:
+	cudaFree(GPUImageData);
+
+	return renderTime;
 }
